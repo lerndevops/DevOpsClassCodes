ad5e5cd6ce04b496208a13f505729b5d10071ff6
Augustine Kyei <Augustine.Kyei@senapt.co.uk>
2023-05-20 21:54:37 +0100

    test

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -77,8 +77,8 @@ public class AddressbookUI extends UI {
 
         contactList
                 .setContainerDataSource(new BeanItemContainer<>(Contact.class));
-        contactList.setColumnOrder("firstName", "lastName", "email", "phone");
-        contactList.removeColumn("id");
+        contactList.setColumnOrder("firstName", "lastName", "email", "phone", "id");
+        // contactList.removeColumn("id");
         contactList.removeColumn("birthDate");
         // contactList.removeColumn("phone");
         contactList.setSelectionMode(Grid.SelectionMode.SINGLE);
